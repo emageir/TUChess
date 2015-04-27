487cdb185d772c7e55e4618849830800f9ab938c
vagos06 <vagos000@gmail.com>
2015-04-27 20:07:56 +0300

    Started developing Main

--- src/source/Main.java
+++ src/source/Main.java
@@ -6,7 +6,17 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		//Dhmiourgoume object gia th syndesh
+		Connector conn = new Connector(9876, 200, "Chesster");
 		
+		//Stelnoume to onoma sto server
+		conn.sendName();
+		
+		//Lamvanoume ta mhnymata kai ta epeksergazomaste
+		while(true){
+			
+			
+		}
 
 	}
 
