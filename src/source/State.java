216b3503836e7fc081bff6f810842a6638248fc2
vagos06 <vagos000@gmail.com>
2015-04-29 03:55:50 +0300

    Added children list

--- src/source/State.java
+++ src/source/State.java
@@ -1,5 +1,7 @@
 package source;
 
+import java.util.ArrayList;
+
 public class State {
 	
 	private String[][] board;
@@ -7,7 +9,7 @@ public class State {
 	private int scoreBlack=0;
 	private int lastPlayed=1;/// antistoixa me to color. arxikopoieitai sto 1 giati prwtos paizei o white
 	private int[] lastMove;
-	
+	private ArrayList<State> children = null;
 	private State father;
 	
 	
@@ -16,7 +18,7 @@ public class State {
 		this.father=father;
 		this.lastPlayed=lastPlayed;
 		this.lastMove=lastMove;
-		
+		this.children = new ArrayList<State>();
 		//setScoreWhite();
 		//setScoreBlack();
 	}
@@ -77,6 +79,10 @@ public class State {
 		return lastPlayed;
 	}
 	
+	public ArrayList<State> getChildren(){
+		
+		return children;
+	}
 	
 	public int evaluate(State board){
 		int value=0;
